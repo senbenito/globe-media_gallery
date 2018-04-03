f525bda0d7d9f21f64c024adc89f172c68537603
Shannon Rivers <shannon.rivers@gmail.com>
2018-04-03 12:40:04 -0600

    setup Next

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <Layout>
+    <h1>GLOBE Media Gallery</h1>
+    <ul>
+      {props.media.map(medium => (
+        <li key={medium.id}>
+          <Link as={`/media/${medium.id}`} href={`/media?id=${medium.id}`}>
+            <a>{medium.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Media data fetched. Count: ${data.length}`)
+
+  data.medium = []
+  data.map(e=>{
+     data.medium.push(e.show)
+  })
+
+  return {
+    media: data.medium,
+  }
+}
+
+export default Index
